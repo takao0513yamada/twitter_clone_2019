b042e77e03c09b1054b982bd42af3998568eed09
taka president <life.i.s.w---@i.softbank.jp>
2019-09-14 07:20:42 +0900

    fix tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,5 +13,14 @@ class TweetsController < ApplicationController
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body)
+    end
 end
