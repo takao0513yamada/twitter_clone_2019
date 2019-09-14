2f6b4947eaa10bb9b70694a8bf13da8d9a837ff3
taka president <life.i.s.w---@i.softbank.jp>
2019-09-14 15:01:34 +0900

    Setting association(favorite)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'favorites/create'
-  get 'favorites/destroy'
   root 'tweets#index'
-
   devise_for :users
   resources :tweets
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users
 
 end
