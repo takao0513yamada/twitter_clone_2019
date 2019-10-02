0a3e86d7091669c6d0e76eeed14ff9bfbce4c82e
taka president <life.i.s.w---@i.softbank.jp>
2019-10-02 21:09:57 +0900

    Fix model

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-  
+
   root 'tweets#index'
+  resources :images, except: :index
   devise_for :users
   resources :tweets
   resources :tweets do
     resource :favorites, only: [:create, :destroy]
   end
   resources :users
-  resources :images, except: :index
+
 
 end
