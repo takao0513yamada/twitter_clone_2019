4c145c2e77634e066c254d8fa07627e58900bbb5
taka president <life.i.s.w---@i.softbank.jp>
2019-10-06 10:35:45 +0900

    Create carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 's3-rails-images'
+  config.cache_storage = :fog
+end
